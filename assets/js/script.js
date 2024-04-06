05d0d45c74fe3ac7e643019a250a5717e7553d26
Yusuf Elmas <yenes_656@hotmail.com>
2024-04-06 14:04:40 +0300

    First Project Added

---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+jQuery(document).ready(function ($) {
+  $("#main-nav").hcOffcanvasNav({
+    levelOpen: 'none',
+    disableAt: 992,
+    levelTitles: true,
+    levelTitleAsBack: true,
+  });
+});
+
+var swiper = new Swiper(".iconSwiper", {
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+var swiper = new Swiper(".cardSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".newsSwiper", {
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".teamSwiper", {
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+  },
+});
